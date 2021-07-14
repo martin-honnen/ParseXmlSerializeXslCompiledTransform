0e1c030bd90235fa7876099d4d20d0faa8399543
Martin Honnen <martin.honnen@gmx.de>
2021-07-14 12:49:17 +0200

    Projektdateien hinzufügen.

---
+++ ParseXmlSerializeXslCompiledTransform/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace ParseXmlSerializeXslCompiledTransform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xml = @"<re>
+  <bl><![CDATA[&#60;ul&#62;&#60;li&#62;Linienfl&#252;ge&#60;/li&#62;&#60;/ul&#62;]]></bl>
+</re>";
+
+            XslCompiledTransform processor = new XslCompiledTransform();
+
+            processor.Load("XSLTFile1.xslt");
+
+            XsltArgumentList xsltArgumentList = new XsltArgumentList();
+            xsltArgumentList.AddExtensionObject("http://example.com/mf", new MyExtensions());
+
+            using (StringReader sr = new StringReader(xml))
+            {
+                using (XmlReader xr = XmlReader.Create(sr))
+                {
+                    processor.Transform(xr, xsltArgumentList, Console.Out);
+                }
+            }
+        }
+    }
+
+    public class MyExtensions
+    {
+        public static XPathNavigator ParseXmlFragment(string xml)
+        {
+            using (StringReader sr = new StringReader(xml))
+            {
+                using (XmlReader xr = XmlReader.Create(sr, new XmlReaderSettings() { ConformanceLevel = ConformanceLevel.Fragment }))
+                return new XPathDocument(xr).CreateNavigator();
+            }
+        }
+
+        public static string Serialize(XPathNavigator node)
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                using (XmlWriter xw = XmlWriter.Create(sw, new XmlWriterSettings() { OmitXmlDeclaration = true, Indent = false }))
+                {
+                    node.WriteSubtree(xw);
+               
+                }
+                return sw.ToString();
+            }
+        }
+    }
+
+}
---
+++ ParseXmlSerializeXslCompiledTransform/XSLTFile1.xslt
@@ -0,0 +1,14 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:mf="http://example.com/mf"
+  exclude-result-prefixes="mf">
+  
+  <xsl:output method="xml" cdata-section-elements="text"/>
+
+  <xsl:template match="re">
+    <text>
+      <xsl:value-of select="mf:Serialize(mf:ParseXmlFragment(mf:ParseXmlFragment(bl)))"/>
+    </text>
+  </xsl:template>
+  
+</xsl:stylesheet>
